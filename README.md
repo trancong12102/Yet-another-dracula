5931f24a2cfe7eeeced3e703fe0910398bc50c42
Trần Công <nopain2110@gmail.com>
2020-05-01 11:53:58 +0700

    Add instructions

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Yet-another-dracula
+# Yet another dracula theme for KDE Plasma Desktop
+Based on vinceliuice's [Layan KDE](https://github.com/vinceliuice/Layan-kde) and EliverLara's [Ant-Dracula](https://github.com/EliverLara/Ant-Dracula/tree/master/kde) <br/>
+This repository contains:
+- Plasma desktop theme
+- Kvantum theme
+- Plasma color scheme
+- SDDM theme
+
+# Installation
+1. Install [hello decoration](https://github.com/n4n0GH/hello)
+
+Buttons color scheme: 
+- close: `#ff5555`
+- minimize: `#f1fa8c`
+- maximize: `#50fa7b`
+
+1. Install Yet-another-dracula theme
+```bash
+$ git clone https://github.com/nopain2110/Yet-another-dracula.git
+``` 
+- Copy `color-schemes` to `~/.local/share/color-schemes` and activate by go to `Settings → Colors → Yet-another-dracula`
+- Copy `Kvantum` to `~/.config/Kvantum` and activate by go to `Settings → Application Style → kvantum`, open Kvantum Manager and select `Yet-another-dracula`
+- Copy `plasma` to `~/.local/share/plasma` and activate by go to `Settings → Plasma Style → Yet-another-dracula`
+- Copy `sddm` to `~/.local/share/sddm` and activate by go to `Settings → Startup and Shutdown → Login Screen (SDDM) → Dracula`
+  
+# Recommendations
+- For better looking please use this pack with Kvantum engine.
+
+    Run kvantummanager to choose and apply Yet-another-dracula theme.
+
+- Install vinceliuice's [Tela icon theme](https://github.com/vinceliuice/Tela-icon-theme) with purple dark variant and patch colors with my script [dracula-tela-icon-patch.sh](https://github.com/nopain2110/Yet-another-dracula/Yet-another-dracula/dracula-tela-icon-patch.sh) for a more consistent and beautiful experience.
+
+# License
+GNU GPL v3
+
+# Preview
+![](https://github.com/nopain2110/Yet-another-dracula/blob/master/previews/1.png?raw=true)
+![](https://github.com/nopain2110/Yet-another-dracula/blob/master/previews/2.png?raw=true)
+![](https://github.com/nopain2110/Yet-another-dracula/blob/master/previews/3.png?raw=true)
+![](https://github.com/nopain2110/Yet-another-dracula/blob/master/previews/4.png?raw=true)
---
+++ Yet-another-dracula/dracula-tela-icon-patch.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+### PATH
+readonly ICONS="$HOME/.local/share/icons"
+readonly PATCH_PATHS=(
+	"$ICONS/Tela"
+	"$ICONS/Tela-dark"
+	"$ICONS/Tela-purple"
+	"$ICONS/Tela-purple-dark"
+)
+### COLOR PALETTE
+readonly BACKGROUND=#282a36
+readonly CURRENT_LINE=#44475a
+readonly FOREGROUND=#f8f8f2
+readonly PURPLE_DARK_1=#7c60a3
+readonly PURPLE=#bd93f9
+readonly COMMENT_DARK_1=#566490
+readonly COMMENT=#6272a4
+readonly COMMENT_LIGHT_1=#7184bd
+readonly CYAN=#8be9fd
+readonly GREEN=#50fa7b
+readonly ORANGE=#ffb86c
+readonly ORANGE_DARK_1=#e6a661
+readonly PINK=#ff79c6
+readonly RED=#ff5555
+readonly YELLOW=#f1fa8c
+
+### Replacements
+## Purple
+readonly PURPLE_RPS=('#7e57c2' '#5294e2')
+## Purple dark 1
+readonly PURPLE_DARK_1_RPS=('#3578c7')
+## Blue
+readonly BLUE_RPS=('#498cd8' '#608cc5' '#324090' '#53a6bc' '#209ae7' '#2e6ea9' '#5091dd' '#71bc42' '#8dd786' '#368eb7')
+## Blue light 1
+readonly BLUE_LIGHT_1_RPS=('#71a5e8' '#5ca4ed' '#5ca0d6' '#6390cb' '#3fa7d7')
+## Cyan
+readonly CYAN_RPS=('#a0ecff' '#c0f957')
+## Green
+readonly GREEN_RPS=()
+## Red
+readonly RED_RPS=('#cf000f' '#da4453')
+## Orange
+readonly ORANGE_RPS=('#5da3ee')
+## Orange dark 1
+readonly ORANGE_DARK_1_RPS=('#3e74b7')
+
+patch_color () {
+	need_patch_color=$1
+	patch_color=$2
+
+	for path in "${PATCH_PATHS[@]}"; do
+    	find "$path" \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/$need_patch_color/$patch_color/g"
+	done
+}
+
+echo 'Patching purple colors...'
+for color in "${PURPLE_RPS[@]}"; do
+	echo "-> replace $color with $PURPLE"
+	patch_color $color $PURPLE
+done
+for color in "${PURPLE_DARK_1_RPS[@]}"; do
+	echo "-> replace $color with $PURPLE_DARK_1"
+	patch_color $color $PURPLE
+done
+echo 'Patching blue colors...'
+for color in "${BLUE_RPS[@]}"; do
+	echo "-> replace $color with $COMMENT"
+	patch_color $color $COMMENT
+done
+for color in "${BLUE_LIGHT_1_RPS[@]}"; do
+	echo "-> replace $color with $COMMENT_LIGHT_1"
+	patch_color $color $COMMENT_LIGHT_1
+done
+echo 'Patching cyan colors...'
+for color in "${CYAN_RPS[@]}"; do
+	echo "-> replace $color with $CYAN"
+	patch_color $color $CYAN
+done
+echo 'Patching green colors...'
+for color in "${GREEN_RPS[@]}"; do
+	echo "-> replace $color with $GREEN"
+	patch_color $color $GREEN
+done
+echo 'Patching red colors...'
+for color in "${RED_RPS[@]}"; do
+	echo "-> replace $color with $RED"
+	patch_color $color $RED
+done
+echo 'Patching orange colors...'
+for color in "${ORANGE_RPS[@]}"; do
+	echo "-> replace $color with $ORANGE"
+	patch_color $color $ORANGE
+done
+for color in "${ORANGE_DARK_1_RPS[@]}"; do
+	echo "-> replace $color with $ORANGE_DARK_1"
+	patch_color $color $ORANGE_DARK_1
+done
+echo 'Done!'
